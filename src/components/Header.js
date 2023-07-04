d47f8a3522e458486d0b521ada6a6f7390f150c8
polin103109 <sabrinpolinece18@gmail.com>
2023-07-04 11:45:54 +0600

    added logo in header, scroll functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+//logos
+import LogoWhite from "../assets/img/logo-white.svg";
+import LogoDark from "../assets/img/logo-dark.svg";
 const Header = () => {
-  return <div> Header</div>;
+  const [header, setHeader] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      window.scrollY > 50 ? setHeader(true) : setHeader(false);
+    });
+  });
+
+  return (
+    <header
+      className={`${
+        header ? "bg-white py-6 shadow-lg" : "bg-transparent py-8"
+      } fixed w-full z-50 transition-all duration-300`}
+    >
+      <div className="container mx-auto">
+        <a href="/">
+          {header ? <img src={LogoDark} /> : <img src={LogoWhite} />}
+        </a>
+      </div>
+    </header>
+  );
 };
 export default Header;
