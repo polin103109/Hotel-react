1f764e9527848fdac381e1830a9595713812cd1d
polin103109 <sabrinpolinece18@gmail.com>
2023-07-05 11:11:01 +0600

    added the footer section and edited heroslider

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,15 @@
 import React from "react";
+import LogoWhite from "../assets/img/logo-white.svg";
 const Footer = () => {
-  return <div> Footer</div>;
+  return (
+    <footer className="bg-primary py-12">
+      <div className="container mx-auto text-white flex justify-between">
+        <a href="/">
+          <img src={LogoWhite} alt="" />
+        </a>
+        Copyright &copy; 2023. All rights reserved.
+      </div>
+    </footer>
+  );
 };
 export default Footer;
--- src/components/HeroSlider.js
+++ src/components/HeroSlider.js
@@ -1,9 +1,71 @@
 import React from "react";
+//import swiper component
+import { Swiper, SwiperSlide } from "swiper/react";
+//import swiper styles
+import "swiper/css";
+import "swiper/css/effect-fade";
+//import required module
+import { EffectFade, Autoplay } from "swiper";
+//import images
+import Img1 from "../assets/img/heroSilder/1.jpg";
+import Img2 from "../assets/img/heroSilder/2.jpg";
+import Img3 from "../assets/img/heroSilder/3.jpg";
+
+const slides = [
+  {
+    title: "Your Luxury Hotel for Vacation",
+    bg: Img1,
+    btnText: "See our rooms",
+  },
+  {
+    title: "Your Luxury Hotel for Vacation",
+    bg: Img2,
+    btnText: "See our rooms",
+  },
+  {
+    title: "Your Luxury Hotel for Vacation",
+    bg: Img3,
+    btnText: "See our rooms  ",
+  },
+];
 const HeroSlider = () => {
   return (
-    <div className="heroSlider h-[600px] lg:h-[860px]  bg-black">
-      HeroSlider
-    </div>
+    <Swiper
+      modules={[EffectFade, Autoplay]}
+      effect={"fade"}
+      Loop={true}
+      autoplay={{ delay: 3000, disableOnInteraction: false }}
+      className="heloSlider h-[600px] lg:h-[860px]"
+    >
+      {slides.map((slide, index) => {
+        //destructure slides
+        const { title, bg, btnText } = slide;
+        return (
+          <SwiperSlide
+            className="h-full relative flex justify-center items-center"
+            key={index}
+          >
+            <div className="absolute top-0 w-full h-full">
+              <img className="object-cover w-full h-full" src={bg} alt="" />
+            </div>
+            {/* overlay*/}
+            <div className="absolute w-full h-full bg-black/70">
+              {/* <div className="z-20 text-white text-center absolute ">
+                <div className="uppercase font-tertiary tracking-[6px] absolute">
+                  Just Enjoy & relax
+                </div>
+                <h1 className="text-[32px] font-primary uppercase tracking-[2px] max-w-[920px] lg:text-[68px] leading-tight mb-6">
+                  {title}
+                </h1>
+                <button className="btn btn-lg btn-primary mx-auto">
+                  {btnText}
+                </button> */}
+              {/* </div> */}
+            </div>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
   );
 };
 export default HeroSlider;
